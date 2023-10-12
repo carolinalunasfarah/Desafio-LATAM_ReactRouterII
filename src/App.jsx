a8bcf888fef986bae93a9c141b88829e15d0c8bc
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-10-12 11:25:42 -0300

    create routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
-  
+    return (
+        <>
+            <div>
+                <Navigation />
 
-  return (
-    <>
-      
-    </>
-  )
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/pokemons" element={<Pokemons />} />
+                    <Route
+                        path="/pokemons/:name"
+                        element={<PokemonDetails />}
+                    />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
