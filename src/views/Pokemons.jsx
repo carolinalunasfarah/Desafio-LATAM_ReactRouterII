6e2e21f0c4aa1a081426da71bfcc8207c860ffaf
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-10-12 12:32:30 -0300

    modify view Pokemons to show select

--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -1,9 +1,31 @@
+import { useContext } from "react";
+import { PokemonsContext } from "../context/PokemonContext";
+
 const Pokemons = () => {
+    const { pokemons, selectedPokemons, setSelectedPokemons, setPokemons } =
+        useContext(PokemonsContext);
 
-  return (
-    <div>
-    </div>
-  )
-}
+    return (
+        <div>
+            <h1>Select a Pokemon</h1>
+            <section>
+                <select
+                    value={selectedPokemons}
+                    onChange={({ target }) =>
+                        setSelectedPokemons(target.value)
+                    }>
+                    <option value="" disabled>
+                        Pokemons
+                    </option>
+                    {pokemons.map(({ name }, i) => (
+                        <option key={i} value={name}>
+                            {name}
+                        </option>
+                    ))}
+                </select>
+            </section>
+        </div>
+    );
+};
 
-export default Pokemons
+export default Pokemons;
