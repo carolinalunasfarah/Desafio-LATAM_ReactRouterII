f5413d4c3805712f79aa172bf54f8257d3b2cd8a
Carolina Lunas <carolinalunasfarah@gmail.com>
2023-10-12 16:34:21 -0300

    modify navigation style

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,14 +4,20 @@ import Navbar from "react-bootstrap/Navbar";
 import pokeIcon from "../assets/img/poke-icon.png";
 
 const Navigation = () => {
-    const activeClass = ({ isActive }) => (isActive ? "active" : undefined);
+    const activeClass = ({ isActive }) => (isActive ? "active" : "inactive");
 
     return (
-        <Navbar className="bg-body-tertiary">
-            <Navbar.Brand>
-                <img src={pokeIcon} alt="Pokemon icon lines" />
-            </Navbar.Brand>
+        <Navbar className="navigation">
             <section>
+                <Navbar.Brand className="navBrand">
+                    <img
+                        className="navIcon"
+                        src={pokeIcon}
+                        alt="Pokemon icon lines"
+                    />
+                </Navbar.Brand>
+            </section>
+            <section className="navLinks">
                 <NavLink className={activeClass} to="/">
                     Home
                 </NavLink>
